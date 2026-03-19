73249317a35d0134def352c60d8a14f958befd61
Rekha Joshi <rekha.joshi2@gmail.com>
2026-03-19 16:05:37 -0700

    Set up project structure and define initial Pydantic schemas for customers, products, orders, and payments

---
+++ app/schemas.py
@@ -0,0 +1,26 @@
+#Schemas define the structure and validation rules for API request and response data.
+#It defines what API expects and ensures that only valid data is accepted.
+
+from pydantic import BaseModel, EmailStr
+from typing import List
+
+class CustomerCreate(BaseModel): #creating a class and it's inherting from BaseModel to validate + parse
+    name: str
+    email: EmailStr
+
+class ProductCreate(BaseModel):
+    name: str
+    price: float
+    stock: int
+
+class OrderItemCreate(BaseModel):
+    order_id: int
+    quantity: int
+
+class OrderCreate(BaseModel):
+    customer_id: int
+    items: List[OrderItemCreate]
+
+class PaymentCreate(BaseModel):
+    order_id: int
+    amount: float
