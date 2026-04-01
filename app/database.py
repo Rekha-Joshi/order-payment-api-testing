921820bd9ea78720faf3d34ba372e4343067c748
Rekha Joshi <rekha.joshi2@gmail.com>
2026-04-01 11:00:30 -0700

    Configure PostgreSQL connection, and create Customer ORM model

---
+++ app/database.py
@@ -0,0 +1,9 @@
+# This file sets up the PostgreSQL connection, session factory, and base class for ORM models.
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, declarative_base
+
+DATABASE_URL = "postgresql://localhost/order_payment_db" #tells Python which DB to connect to
+
+engine = create_engine(DATABASE_URL) #creates DB connection engine
+SessionLocal = sessionmaker(bind=engine) #used later to talk to DB
+Base = declarative_base() #base class for ORM models
---
+++ app/models.py
@@ -0,0 +1,12 @@
+from sqlalchemy import Column, Integer, String, DateTime
+from datetime import datetime, timezone
+
+from app.database import Base
+
+class Customer(Base):
+    __tablename__ = "customers"
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String, nullable=False)
+    email = Column(String, unique=True, nullable=False)
+    created_at = Column(DateTime, default=lambda: datetime.now(timezone.utc))
+
